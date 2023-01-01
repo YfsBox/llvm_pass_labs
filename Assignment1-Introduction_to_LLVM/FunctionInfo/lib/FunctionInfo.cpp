f113bb2e936a29cb0f710e449a1621ecfc5a9c8d
YfsBox <1027342142@qq.com>
2023-01-01 16:43:14 +0800

    feat(assignment1/FunctionInfo.cpp): 实现FunctionInfo

--- Assignment1-Introduction_to_LLVM/FunctionInfo/lib/FunctionInfo.cpp
+++ Assignment1-Introduction_to_LLVM/FunctionInfo/lib/FunctionInfo.cpp
@@ -7,6 +7,8 @@ using namespace llvm;
 namespace {
 
 class FunctionInfo final : public ModulePass {
+private:
+    void showFuncInfo(const Function &func);
 public:
   static char ID;
 
@@ -20,14 +22,22 @@ public:
   virtual bool runOnModule(Module &M) override {
     outs() << "CSCD70 Function Information Pass"
            << "\n";
-
     /**
      * @todo(cscd70) Please complete this method.
      */
-
+    for (auto &func : M) {
+        showFuncInfo(func);
+    }
     return false;
   }
 }; // class FunctionInfo
+
+void FunctionInfo::showFuncInfo(const Function &func) {
+    func.arg_size();
+    outs() << "#Name:" << func.getName() << "\nArgs:" << func.arg_size() << "\nCalls:"
+            << func.getNumUses() << "\nBlocks:" << func.size() << "\nInsts:"
+            << func.getInstructionCount() << "\n";
+}
 
 char FunctionInfo::ID = 0;
 RegisterPass<FunctionInfo> X("function-info", "CSCD70: Function Information");
