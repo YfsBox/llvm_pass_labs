342db2c00f7f4ea19d77ef27f2a85b11fe8c9d38
YfsBox <1027342142@qq.com>
2023-01-04 20:55:09 +0800

    feat: 初步实现LICM

--- Assignment3-Loop_Invariant_Code_Motion/lib/LICM.cpp
+++ Assignment3-Loop_Invariant_Code_Motion/lib/LICM.cpp
@@ -1,6 +1,7 @@
 /**
  * @file Loop Invariant Code Motion
  */
+#include <llvm/IR/Dominators.h>
 #include <llvm/Analysis/LoopPass.h>
 #include <llvm/Analysis/ValueTracking.h>
 #include <llvm/IR/Instruction.h>
@@ -13,22 +14,26 @@ namespace {
 
 class LoopInvariantCodeMotion final : public LoopPass {
 private:
-    bool isInvariant(Instruction *const I, const LoopInfo &loopInfo);
-    bool traversalLoop(Loop *L, const LoopInfo &loopInfo);  // 返回值表示的是这一轮循环有没有出现changed
+    bool isInvariant(const Loop *loop, Instruction *const I);
+    bool traversalLoop(Loop *L);  // 返回值表示的是这一轮循环有没有出现changed
     bool isInSet(const Instruction *I) {
         return InvariantSet.find(const_cast<Instruction*>(I)) != InvariantSet.end();
     }
-
-    std::unordered_set<Instruction*> InvariantSet;
+    bool checkIsExitsDom(const Loop *loop, const Instruction *Inst);
+    void moveInvariant(const Loop *loop, Instruction *Inst);
+    const LoopInfo *loopInfo;
+    const DominatorTree *dominatorTree;
+    std::unordered_set<Instruction*> InvariantSet;  // 这个map应该只让其作用于某一个Loop还是整个Function都有效呢？应该作为每次的Pass也是可以的吧
 public:
   static char ID;
 
-  LoopInvariantCodeMotion() : LoopPass(ID) {}
+  LoopInvariantCodeMotion() : LoopPass(ID), loopInfo(nullptr), dominatorTree(nullptr) {}
 
   virtual void getAnalysisUsage(AnalysisUsage &AU) const override {
     /**
      * @todo(cscd70) Request the dominator tree and the loop simplify pass.
      */
+    AU.addRequired<DominatorTreeWrapperPass>();     // 用于检查某个Instruction是否是支配结点
     AU.addRequired<LoopInfoWrapperPass>();
     AU.setPreservesCFG();       // 保留之前所求出的数据流图的结果.
   }
@@ -40,43 +45,47 @@ public:
       if (L->getLoopPreheader() == nullptr) {
           return false;
       }
-      LoopInfo &loopInfo = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-      // 首先需要从中找出循环不变量，通过遍历Instruction来确定， 收集到一个list中
-      while (traversalLoop(L, loopInfo)) {
-
+      InvariantSet.clear();
+      if (loopInfo == nullptr) {
+          loopInfo = &(getAnalysis<LoopInfoWrapperPass>().getLoopInfo());
       }
-
-
-      // 然后将循环不变量所对应的指令以单个block的形式移动到该Loop的开头
-
-      // 首先需要创建一个block(根据list创建)，该blcok指向循环的header
-
-      // 将原本header的前驱block指向移动为新创建的block
-
+      if (dominatorTree == nullptr) {
+          dominatorTree = &(getAnalysis<DominatorTreeWrapperPass>().getDomTree());
+      }
+      // 首先需要从中找出循环不变量，通过遍历Instruction来确定， 收集到一个list中
+      while (traversalLoop(L)) {
+      }
+      for (auto Invariant : InvariantSet) { // 将对应的指令移动到Preheader中
+          // 需要判断的是是否是exit的支配结点
+          if (checkIsExitsDom(L, Invariant)) {
+              moveInvariant(L, Invariant);
+          }
+      }
+      // 接下来根据所收集的指令判断是否可以并进行移动
       return false;
   }
 };
 
-bool LoopInvariantCodeMotion::isInvariant(Instruction *const I, const LoopInfo& loopInfo) {
+bool LoopInvariantCodeMotion::isInvariant(const Loop *loop, Instruction *const I) { // loop 也正是当前所处理的I所处于loop
 
     bool IsVariable = true;
-    auto isInSameLoop
-    = [&](const Instruction* LInst, const Instruction* RInst) -> bool {
-        return loopInfo.getLoopFor(LInst->getParent())
-        == loopInfo.getLoopFor(RInst->getParent());
+    auto isOutLoop
+    = [&](const Instruction* inst) -> bool {
+        return loop->contains(inst);// 用于判断是否处于统一循环中，从最深的一层开始
     };
 
     for (auto oper_it = I->op_begin(); oper_it != I->op_end(); ++oper_it) {
         bool op_is_const, op_is_inset, op_notsame_loop;
-        op_is_const = isa<Constant>(oper_it);
+        op_is_const = isa<Constant>(oper_it) || isa<Argument>(oper_it);  // 常量和函数参数都是可以的
         auto oper_inst = dyn_cast<Instruction>(oper_it);
         if (oper_inst != nullptr) {
             op_is_inset = isInSet(oper_inst);
+            op_notsame_loop = !isOutLoop(oper_inst);
         } else {
             op_is_inset = false;
+            op_notsame_loop = false;
         }
         // 判断循环所处的位置，是否位于循环外部
-        op_notsame_loop = !isInSameLoop(I, oper_inst);
         IsVariable &= (op_is_inset || op_is_const || op_notsame_loop);
     }
     return IsVariable &&
@@ -86,11 +95,14 @@ bool LoopInvariantCodeMotion::isInvariant(Instruction *const I, const LoopInfo& 
 
 }
 
-bool LoopInvariantCodeMotion::traversalLoop(Loop *L, const LoopInfo &loopInfo) {
+bool LoopInvariantCodeMotion::traversalLoop(Loop *L) {    // 每次只针对某一特定的L, 对应了runOnLoop中的L
     bool is_changed = false;
     for (auto BB : L->getBlocks()) {
+        if (loopInfo->getLoopFor(BB) != L) {     // 每次只针对特定的L
+            continue;
+        }
         for (auto &Inst : *BB) {
-            if (isInvariant(&Inst, loopInfo)) {
+            if (!isInSet(&Inst) && isInvariant(L, &Inst)) {
                 InvariantSet.insert(&Inst);     // 加入到集合中
                 is_changed = true;
             }
@@ -99,6 +111,25 @@ bool LoopInvariantCodeMotion::traversalLoop(Loop *L, const LoopInfo &loopInfo) {
     return is_changed;
 }
 
+bool LoopInvariantCodeMotion::checkIsExitsDom(const Loop *loop, const Instruction *Inst) {
+    // 在一个控制流图中, 其Exit Block往往有好几个
+    SmallVector<BasicBlock*, 0> exit_blocks;
+    loop->getExitBlocks(exit_blocks);
+    for (auto exit_block : exit_blocks) {
+        // 检查Inst是否为该Block的支配结点
+        if (!dominatorTree->dominates(Inst, exit_block)) { // 如果不是支配结点
+            return false;
+        }
+    }
+    return true;
+}
+
+void LoopInvariantCodeMotion::moveInvariant(const Loop *loop, Instruction *Inst) {
+    auto perheader = loop->getLoopPreheader();
+    Inst->moveBefore(perheader->getTerminator());
+}
+
+
 char LoopInvariantCodeMotion::ID = 0;
 RegisterPass<LoopInvariantCodeMotion> X("loop-invariant-code-motion",
                                         "Loop Invariant Code Motion");
@@ -106,3 +137,6 @@ RegisterPass<LoopInvariantCodeMotion> X("loop-invariant-code-motion",
 } // anonymous namespace
 
 // 关于Loop这个class及其相关class的分析
+// 其中关于LoopPass是每次只处理一个Loop单元, Loop在一个程序中是一个区分层次的结构，在对内部的指令进行遍历时需要确保
+// 处理的指令一定处于该Loop之下，如果不是就跳过。
+//
